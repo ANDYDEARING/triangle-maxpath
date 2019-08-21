b9cd0a2aecc0d04ee3287e9ea328d443a5daee1b
Andy Dearing <andy.dearing@gmail.com>
2019-08-21 11:44:35 -0600

    finished problem

--- max_path.py
+++ max_path.py
@@ -1,1 +1,18 @@
 triangle = [[3],[7,4],[2,4,6],[8,5,9,3]]
+
+num_rows = len(triangle)
+row_index = num_rows - 2
+
+while row_index >= 0:
+    column_index = 0
+    for number in triangle[row_index]:
+        down_left = number + triangle[row_index+1][column_index]
+        down_right = number + triangle[row_index+1][column_index+1]
+        if down_left > down_right:
+            triangle[row_index][column_index] = down_left
+        else:
+            triangle[row_index][column_index] = down_right
+        column_index += 1
+    row_index -= 1
+
+print(triangle[0][0])
